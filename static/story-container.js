a42a74f8f004636ae667df99373ed14b1a508363
minjeong07 <minnjeong07@gmail.com>
2021-12-31 14:47:56 +0900

    [Add] story-container files(html, css, js)

---
+++ static/story-container.js
@@ -0,0 +1,33 @@
+const slides = document.querySelector('#story-slide');
+const slide = document.querySelectorAll('.story-wrapper');
+let currentIdx = 0;
+const slideCount = slide.length;
+const prevBtn = document.querySelector('#prev');
+const slideWidth = 4.3;
+const slideMargin = 1.2;
+const nextBtn = document.querySelector('#next');
+
+slides.style.width = (slideWidth + slideMargin) * slideCount - slideMargin + 'rem';
+
+function moveSlide(num) {
+    slides.style.left = -num * 5.5 + 'rem';
+    currentIdx = num;
+}
+
+nextBtn.addEventListener('click', function () {
+    if (currentIdx < slideCount - 5) {
+        moveSlide(currentIdx + 1)
+    }
+});
+prevBtn.addEventListener('click', function () {
+    if (currentIdx > 0) {
+        moveSlide(currentIdx - 1);
+    }
+});
+
+$(document).ready(function () {
+    $(".story-on").click(function () {
+        $(this).removeClass("story-on");
+        $(this).addClass("story-off");
+    });
+});
