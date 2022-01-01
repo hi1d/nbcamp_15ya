94688f178c9d80fb30225d40b15853c457f83700
gavin-ko <gavin.meattiger@gmail.com>
2022-01-02 02:17:59 +0900

    [add] index and source files
    -add search function in app.py
    -change grid layout(index.html : header ,left column feed)
    -add  content (index.html : right column content)
    -add css, js files
    -create icon folder and add SVG files(P.S. Please chat if you change)

--- app.py
+++ app.py
@@ -426,6 +426,15 @@ def bookmark():
     except TypeError:
         return redirect(url_for('login'))
 
+# 검색 기능 ############ 수정 요청 1. 검색 시 나오는 user_list(프로필 이미지, 아이디 or 닉네임)################
+#############################  2. ../js/index.js ajex부분, function searching() 확인 요청 ################
+@app.route("/users", methods=["GET"])
+def users_search():
+    user_list = list(db.users.find({},{'_id': False}))
+
+    return jsonify({'users': user_list})
+# 최근 검색 기록 : 차후 수정 예정
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
