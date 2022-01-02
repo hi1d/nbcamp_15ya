c9a3c36569196072db7d255176bdc81cc7532852
minjeong07 <minnjeong07@gmail.com>
2022-01-02 20:59:48 +0900

    [fix] story-page

--- static/js/story-page.js
+++ static/js/story-page.js
@@ -6,11 +6,10 @@ const prevBtn = document.querySelector('#prev');
 const slideWidth = 17;
 const nextBtn = document.querySelector('#next');
 
-    slides.style.width = (slideWidth * slideCount) + 'rem';
+slides.style.width = (slideWidth * slideCount) + 'rem';
 
-const prevPage = document.getElementById('prev').alt
-const nextPage = document.getElementById('next').alt
-const imgBorder = document.querySelector('.story-off')
+const prevPage = document.getElementById('prev').alt;
+const nextPage = document.getElementById('next').alt;
 
 function moveSlide(num) {
     slides.style.left = -num * 17 + 'rem';
@@ -20,17 +19,33 @@ function moveSlide(num) {
 nextBtn.addEventListener('click', function () {
     if (currentIdx < slideCount - 1) {
         moveSlide(currentIdx + 1)
-    }
-    else if (currentIdx == slideCount -1) {
+    } else if (currentIdx == slideCount - 1) {
+
+        let offId = $('#user-nick').html()
+        let offProfile = $('#user-profile').attr('src')
+        let offImg = $('._img').attr('src')
+        let form_data = new FormData()
+
+        form_data.append('id', offId)
+        form_data.append('profile', offProfile)
+        form_data.append('story_img_url',offImg)
+
+        $.ajax({
+            type: 'POST',
+            url: '/off-list/add',
+            data: form_data,
+            cache: false,
+            contentType: false,
+            processData: false,
+        });
         window.location.href = nextPage
     }
-});
+})
+;
 prevBtn.addEventListener('click', function () {
     if (currentIdx > 0) {
         moveSlide(currentIdx - 1);
-    }
-    else if (currentIdx == 0) {
+    } else if (currentIdx == 0) {
         window.location.href = prevPage
     }
 });
-
